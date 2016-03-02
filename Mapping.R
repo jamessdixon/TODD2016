4bc7636a6aca6b24fe6fd5fa647b04084f124554
Jamie Dixon <jamessdixon@gmail.com>
2016-03-02 08:42:33 -0500

    Initial commit

---
+++ Mapping.R
@@ -0,0 +1,71 @@
+data.set <- maml.mapInputPort(1) # class: data.frame
+data.set[,5:8] <- !is.na(data.set[,5:8])
+maml.mapOutputPort("data.set");
+
+
+
+
+You do know, that just under half of all data science jokes are below average, right?
+On being heckled for a below average data science joke, a fun retort is "Well, I guess that's proof that average people can often be mean."
+
+
+A statistician’s wife had twins. He was delighted. He rang the minister who was also delighted. “Bring them to church on Sunday and we’ll baptize them,” said the minister. “No,” replied the statistician. “Baptize one. We’ll keep the other as a control.”
+
+———————————-:D—:D—:D—:D—————————
+
+Two statisticians were traveling in an airplane from LA to New York. About an hour into the flight, the pilot announced that they had lost an engine, but don’t worry, there are three left. However, instead of 5 hours it would take 7 hours to get to New York.
+
+A little later, he announced that a second engine failed, and they still had two left, but it would take 10 hours to get to New York.
+
+Somewhat later, the pilot again came on the intercom and announced that a third engine had died. Never fear, he announced, because the plane could fly on a single engine. However, it would now take 18 hours to get to New York.
+
+At this point, one statistician turned to the other and said, “Gee, I hope we don’t lose that last engine, or we’ll be up here forever!”
+
+———————————-:D—:D—:D—:D—————————
+
+Patient: “Will I survive this risky operation?”
+ Surgeon: “Yes, I’m absolutely sure that you will survive the operation.”
+ Patient: “How can you be so sure?”
+ Surgeon: “9 out of 10 patients die in this operation, and yesterday died my ninth patient.”
+
+———————————-:D—:D—:D—:D—————————
+
+One day there was a fire in a wastebasket in the office of the Dean of Sciences. In rushed a physicist, a chemist, and a statistician. The physicist immediately starts to work on how much energy would have to be removed from the fire to stop the combustion.
+
+The chemist works on which reagent would have to be added to the fire to prevent oxidation. While they are doing this, the statistician is setting fires to all the other wastebaskets in the office.
+
+“What are you doing?” the others demand. The statistician replies, “Well, to solve the problem, you obviously need a larger sample size.”
+
+———————————-:D—:D—:D—:D—————————
+
+Three statisticians went out hunting, and came across a large deer. The first statistician fired, but missed, by a meter to the left. The second statistician fired, but also missed, by a meter to the right. The third statistician didn’t fire, but shouted in triumph, “On the average we got it!”
+
+———————————-:D—:D—:D—:D—————————
+
+A statistics major was completely hung over the day of his final exam. It was a true/false test, so he decided to flip a coin for the answers. The statistics professor watched the student the entire two hours as he was flipping the coin … writing the answer … flipping the coin … writing the answer.
+
+At the end of the two hours, everyone else had left the final except for the one student. The professor walks up to his desk and interrupts the student, saying, “Listen, I have seen that you did not study for this statistics test, you didn’t even open the exam. If you are just flipping a coin for your answer, what is taking you so long?” The student replies bitterly (as he is still flipping the coin), “Shhh! I am checking my answers!
+
+———————————-:D—:D—:D—:D—————————
+
+A company manager is flying across the desert in a hot air balloon when he realizes he is lost. He calls down to a man riding a camel below him and asks where he is.
+
+The man replies “You’re 42 degrees and 12 minutes, 21.2 seconds north, 122 degrees , 10 minutes west, 212 metres above sea level, heading due east by north east.”
+
+“Thanks,” replies the balloonist. “By the way, are you a data analyst?”
+
+“Yes,” replies the man, “how did you know?”
+
+“Everything you told me was totally accurate, you gave me way more information than I needed and I still have no idea what I need to do.”
+
+“I’m sorry,” replied the camel-riding analyst. “By the way, are you a company manager?”
+
+“Yes,” said the balloonist, “how did you know?”
+
+“Well,” replied the analyst, “You’ve got no idea where you are, no idea what direction you’re heading in, you got yourself into this fix by blowing a load of hot air, and now you expect me to get you out of it.”
+
+———————————-:D—:D—:D—:D—————————
+
+Question: What’s the difference between an introverted data analyst & an extroverted one?
+
+Answer: the extrovert stares at YOUR shoes.
